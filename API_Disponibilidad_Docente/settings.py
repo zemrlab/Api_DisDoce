5277e8ec75786879d1f448109a3d804d9219340a
stevematos <s.s.m.m_98@hotmail.com>
2018-04-25 09:26:12 -0500

    Cambios_heroku
    Cambios para subir a heroku

--- API_Disponibilidad_Docente/settings.py
+++ API_Disponibilidad_Docente/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#osbl4*3%_0r2rwr9zix!mrm_=#ugyp$z3#!b)6g1bmgwd5b8v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsPostCsrfMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +76,6 @@ TEMPLATES = [
         },
     },
 ]
-
-WSGI_APPLICATION = 'API_Disponibilidad_Docente.wsgi.application'
 
 
 # Database
@@ -144,4 +143,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- API_Disponibilidad_Docente/wsgi.py
+++ API_Disponibilidad_Docente/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API_Disponibilidad_Docente.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
